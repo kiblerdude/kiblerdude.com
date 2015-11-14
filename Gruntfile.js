169800c67595ae6c9ed1131c91a1eb9b429ff5f3
kiblerdude <kiblerj@gmail.com>
2015-11-14 09:22:28 -0500

    integrate usemin, uglify, and htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,108 @@
-module.exports = function(grunt) {
-
-  grunt.initConfig({
-    // cleanup the last build
-    clean: {
-      build: ["dist/"]
-    },
-    // create a dist directory and copy all the necessary files
-    copy: {
-      main: {
-        expand: true,
-        src: [ "*.html", "js/**", "images/**", "scripts/**" ],
-        dest: "dist/",
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-
-  grunt.registerTask('default', ['clean','copy']);
-
+'use strict';
+ 
+module.exports = function (grunt) {
+ 
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+ 
+    // Configurable paths for the application
+    var appConfig = {
+        app: require('./bower.json').appPath || '.',
+        dist: 'dist'
+    };   
+ 
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+       
+        yeoman: appConfig,
+ 
+        // Deletes all the files in the dist directory to prepare for
+        // a clean build.
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [ '.tmp', 'dist/{,*/}*' ]
+                }]
+            },
+            tmp: '.tmp'
+        },
+ 
+        // Reads HTML for usemin blocks to enable smart builds that automatically
+        // concat, minify and revision files. Creates configurations in memory so
+        // additional tasks can operate on them
+        useminPrepare: {
+            html: '<%= yeoman.app %>/*.html',
+            options: {
+                dest: '<%= yeoman.dist %>',
+                flow: {
+                    html: {
+                        steps: {
+                            js: ['concat', 'uglifyjs'],
+                            css: ['cssmin']
+                        },
+                        post: {}
+                    }
+                }
+            }
+        },
+ 
+        // Performs rewrites based on filerev and the useminPrepare configuration
+        usemin: {
+            html: ['<%= yeoman.dist %>/{,*/}*.html'],
+            css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+            options: {
+                assetsDirs: ['<%= yeoman.dist %>','<%= yeoman.dist %>/images']
+            }
+        },
+ 
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= yeoman.app %>',
+                    dest: '<%= yeoman.dist %>',
+                    src: [
+                        "*.html", "images/**", "js/**"
+                    ]
+                }]
+            }
+        },
+ 
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true,
+                    conservativeCollapse: true,
+                    collapseBooleanAttributes: true,
+                    removeCommentsFromCDATA: true,
+                    removeOptionalTags: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.dist %>',
+                    src: ['*.html'],
+                    dest: '<%= yeoman.dist %>'
+                }]
+            }
+        }
+ 
+    });
+ 
+    grunt.registerTask('build', [
+        'clean:dist',
+        'useminPrepare',
+        'concat:generated',
+        'cssmin:generated',
+        'uglify:generated',
+        'copy:dist',
+        'usemin',
+        'htmlmin:dist',
+        'clean:tmp'
+    ]);
+ 
+    grunt.registerTask('default', [
+        'build'
+    ]);   
 };
