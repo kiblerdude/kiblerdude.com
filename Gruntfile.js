0f007a62dcfa95755ecd99d465dabfd29d8cb1ff
kiblerdude <kiblerj@gmail.com>
2015-11-26 18:06:11 -0500

    use a container for the media section to prevent a horizontal scroll bar from appearing

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function (grunt) {
                     cwd: '<%= yeoman.app %>',
                     dest: '<%= yeoman.dist %>',
                     src: [
-                        "*.html", "images/**", "js/**"
+                        "*.html", "images/**", "js/**", "scripts/**"
                     ]
                 }]
             }
